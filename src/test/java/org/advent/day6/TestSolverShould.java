53e6fed1bac47ab449e364ca91bcb3bf99c0ebf5
gaborweisz <gaborweisz@gmail.com>
2023-12-06 18:31:24 +0000

    day 6 solution

---
+++ src/test/java/org/advent/day6/TestSolverShould.java
@@ -0,0 +1,37 @@
+package org.advent.day6;
+
+import org.advent.day6.Solver;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestSolverShould {
+
+    @Test
+    void calculate_the_solution_for_puzzle_a() {
+
+        //Time:      7  15   30
+        //Distance:  9  40  200
+
+        List<Boat> input = List.of(
+                new Boat(7, 9),
+                new Boat(15, 40),
+                new Boat(30, 200));
+
+        MatcherAssert.assertThat(Solver.solvePuzzleA(input), equalTo(288.0));
+    }
+
+    @Test
+    void calculate_the_solution_for_puzzle_b() {
+
+        //Time:      71530
+        //Distance:  940200
+
+
+        MatcherAssert.assertThat(Solver.solvePuzzleB(new Boat(71530, 940200)), equalTo(71503.0));
+    }
+
+}
